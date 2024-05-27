2c23fe4c86936a5406517b65232a2e225cac4c2d
JooEun-Jeong <prichard147@naver.com>
2024-05-27 20:39:57 +0900

    Show a process UI 6s after clicking search button

--- src/renderer/containers/MainContent/SearchBarArea.tsx
+++ src/renderer/containers/MainContent/SearchBarArea.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useState } from 'react';
 
-import { useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import {
@@ -19,7 +19,7 @@ import { UseSearchApi } from '@apis/hooks/useSearchApi';
 import OpenIm from '@assets/png/openim.png';
 import { SearchField } from '@components';
 import { FindMethods, OpenImDetail, TempPatentDetail } from '@constants';
-import { isMobileAtom } from '@states';
+import { isMobileAtom, isSearchingAtom } from '@states';
 
 import { SearchMainWrapper, SearchBarWrapper, OpenImageWrapper, SearchTitleTypo, AccordionWrapper } from './styled';
 
@@ -29,53 +29,60 @@ export const SearchBarArea = () => {
   const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
   const isMobile = useRecoilValue(isMobileAtom);
 
-  const [text, setText] = useState('');
-
   const navigate = useNavigate();
   const searchApi = UseSearchApi();
 
+  const [text, setText] = useState('');
+  const [isSearching, setIsSearching] = useRecoilState(isSearchingAtom);
+
   const handleSubmit = useCallback(async () => {
     if (searchApi) {
+      setIsSearching(true);
       const data = await searchApi.getLandOwners(text);
       console.log('data ', data);
-      navigate(`/search/${text}`, { state: { keyword: text, data: data } });
+      setTimeout(function () {
+        setIsSearching(false);
+        navigate(`/search/${text}`, { state: { keyword: text, data: data } });
+      }, 6000);
     }
-  }, [navigate, searchApi, text]);
+  }, [navigate, searchApi, setIsSearching, text]);
 
   return (
     <>
-      <SearchMainWrapper>
-        <SearchTitleTypo>조상의 이름으로</SearchTitleTypo>
-        <SearchTitleTypo>몰랐던 내 땅을 찾아보세요!</SearchTitleTypo>
-        <SearchBarWrapper>
-          <SearchField handleSubmit={handleSubmit} setText={setText} />
-        </SearchBarWrapper>
-        <AccordionWrapper>
-          <Accordion>
-            <AccordionSummary expandIcon={<ArrowDropDownIcon />} aria-controls="panel1-content" id="panel1-header">
-              <Typography className="mainTitle">조상땅 찾는 방법</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              {FindMethods.map((item, idx) => (
-                <Box key={`find-method-${idx}`} sx={{ marginBottom: '5px' }}>
-                  <Typography key={`find-method-title-${idx}`} className="methodTitle">
-                    {item.title}
-                  </Typography>
-                  {item.content.map((con, idx2) => (
-                    <Typography key={`find-method-content-${idx}-${idx2}`} className="methodContent">
-                      {con}
+      {!isSearching && (
+        <SearchMainWrapper>
+          <SearchTitleTypo>조상의 이름으로</SearchTitleTypo>
+          <SearchTitleTypo>몰랐던 내 땅을 찾아보세요!</SearchTitleTypo>
+          <SearchBarWrapper>
+            <SearchField handleSubmit={handleSubmit} setText={setText} />
+          </SearchBarWrapper>
+          <AccordionWrapper>
+            <Accordion>
+              <AccordionSummary expandIcon={<ArrowDropDownIcon />} aria-controls="panel1-content" id="panel1-header">
+                <Typography className="mainTitle">조상땅 찾는 방법</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                {FindMethods.map((item, idx) => (
+                  <Box key={`find-method-${idx}`} sx={{ marginBottom: '5px' }}>
+                    <Typography key={`find-method-title-${idx}`} className="methodTitle">
+                      {item.title}
                     </Typography>
-                  ))}
-                </Box>
-              ))}
-            </AccordionDetails>
-          </Accordion>
-          <OpenImageWrapper>
-            <CardMedia component="img" src={OpenIm} />
-            <Typography className="content">{isSmallScreen || isMobile ? TempPatentDetail : OpenImDetail}</Typography>
-          </OpenImageWrapper>
-        </AccordionWrapper>
-      </SearchMainWrapper>
+                    {item.content.map((con, idx2) => (
+                      <Typography key={`find-method-content-${idx}-${idx2}`} className="methodContent">
+                        {con}
+                      </Typography>
+                    ))}
+                  </Box>
+                ))}
+              </AccordionDetails>
+            </Accordion>
+            <OpenImageWrapper>
+              <CardMedia component="img" src={OpenIm} />
+              <Typography className="content">{isSmallScreen || isMobile ? TempPatentDetail : OpenImDetail}</Typography>
+            </OpenImageWrapper>
+          </AccordionWrapper>
+        </SearchMainWrapper>
+      )}
     </>
   );
 };
