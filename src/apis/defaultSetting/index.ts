3fcd9f22ae54243dcef42f2cb006b2e4fad1020f
JooEun-Jeong <prichard147@naver.com>
2024-06-25 15:42:23 +0900

    Add 401 error globally

--- src/apis/defaultSetting/index.ts
+++ src/apis/defaultSetting/index.ts
@@ -21,6 +21,27 @@ export const axiosCreateInstance = (customConfig: AxiosRequestConfig): AxiosInst
   _.merge(config, customConfig);
 
   const instance = axios.create(config);
+  instance.interceptors.response.use(
+    (res) => {
+      return res;
+    },
+    (error) => {
+      if (error.response) {
+        console.log('server responded with non 2xx code: ', error.response.status);
+        console.log('Response data: ', error.response.data);
+        if (error.response.status === 401) {
+          localStorage.removeItem('accessToken');
+          localStorage.removeItem('jwtToken');
+        }
+      } else if (error.request) {
+        console.log('No response received: ', error.request);
+      } else {
+        console.log('Error setting up request: ', error.message);
+      }
+
+      return Promise.reject(error);
+    },
+  );
 
   axiosBetterStacktrace(instance);
 
