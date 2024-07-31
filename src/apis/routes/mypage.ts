01e2663845b43eb82ec451d2e9ae84313ff7b379
JooEun-Jeong <prichard147@naver.com>
2024-07-31 21:01:33 +0900

    Remove unused api: show all payments

--- src/apis/routes/mypage.ts
+++ src/apis/routes/mypage.ts
@@ -1,11 +1,9 @@
 import { AxiosResponse } from 'axios';
 
 import { axiosCreateInstance } from '@apis/defaultSetting';
-import { AxiosHeaderOptions, MyPageContent, MyPageRes, PaymentRes } from '@interfaces/apis';
+import { AxiosHeaderOptions, MyPageRes } from '@interfaces/apis';
 
 export interface AxiosMypageReturn {
-  // getAllPaidLots: (page: number, size: number) => Promise<MyPageRes>;
-  getAllPaymentInfo: (page: number, size: number) => Promise<AxiosResponse<PaymentRes>>;
   getPaidLots: (page: number, size: number, name?: string) => Promise<AxiosResponse<MyPageRes>>;
 }
 
@@ -13,10 +11,6 @@ export const axiosMypage = (opt: AxiosHeaderOptions): AxiosMypageReturn => {
   const { headers } = opt;
   const instance = axiosCreateInstance({ headers });
   return {
-    getAllPaymentInfo: async (page: number, size: number) => {
-      const url = `payment/payment`;
-      return instance.get(url, { params: { status: 'COMPLETED', page: page, size: size } });
-    },
     getPaidLots: async (page: number, size: number, name?: string) => {
       const url = `payment/payment/land-registry`;
 
